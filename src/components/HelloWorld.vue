61c2a6605360ac887cde878ed683ea2dadef61a5
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-02-23 16:42:12 -0600

    last change for 2/23/2021

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,20 +19,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 p{
   background-color: darkgreen;
   color: white;
