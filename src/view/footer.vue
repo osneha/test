1f43eac6ea98ab86d6f0160f125d8c260a1277bd
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-02-23 14:22:17 -0600

    few changes in vue

---
+++ src/view/footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <p>{{ footerText }}</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            footerText : "Copyright NWMSU 2021 "
+        }
+    }
+}
+</script>
+
+<style scoped>
+footer{
+    background-color: lightgreen;
+    padding : 20px;
+    text-align: right;
+    color: black;
+
+}
+</style>
---
+++ src/view/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h3>
+            {{title}}
+        </h3>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title: "NORTHWEST MISSOURI STATE UNIVERSITY"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        background-color: darkgreen;
+        padding: 20px;
+        height: 50px;
+        text-align: left;
+        color: white;
+        
+
+    }
+</style>
